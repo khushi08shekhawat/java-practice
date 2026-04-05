88dd6dcd5b7b022312c101e472b3b6b85ba7501f
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-04-06 05:11:13 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,6 @@
+public class Helloworld { //everyone can access + public class namee would be as same as the file name
+    public static void main(String[]args){
+        System.out.println("Hello world!");
+
+    }
+}
---
+++ IfElse.java
@@ -0,0 +1,13 @@
+public class IfElse
+{
+    public static void main(String[]args){
+        int salary = 25000;
+        if (salary > 10000){
+            salary = salary + 2000;
+        }else {
+            salary = salary + 1000;
+        }
+
+        System.out.println(salary);
+    }
+}
