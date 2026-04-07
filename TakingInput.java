ddc203d90fddd6c53b930b4fe30027529be97fa9
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-04-07 05:41:48 +0530

    Add files via upload

---
+++ TakingInput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class TakingInput {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = sc.next();
+
+        System.out.println("Name is:"+ name);
+
+
+       // int num_1 = sc.nextInt();
+       // System.out.println("Number is:"+ num_1);
+    }
+}
