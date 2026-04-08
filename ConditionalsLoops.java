061363ad8236c4dbe04669128339519f3802927d
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-04-09 04:34:04 +0530

    Add files via upload

---
+++ ConditionalsLoops.java
@@ -0,0 +1,41 @@
+    //public static void main(String[] args) {
+        //print numbers from 1 to 5 using for loop
+        /*for( int num = 1; num<=5; num += 1)
+        {
+            System.out.println(num);
+        }
+
+    }
+    }/* */
+
+
+    // print numbers from 1 to n using while loop
+   import java.util.Scanner;
+public class ConditionalsLoops {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter a number: ");
+//         int n = sc.nextInt();
+//         for(int num =1 ; num <=n ; num++)
+//         {
+//             System.out.print(num);
+//         }
+//     }
+// }
+
+// while loop
+//         int num = 1;
+//         while (num <=5){
+//             System.out.println(num);
+//             num += 1;
+//         }
+//     }
+// }
+//do while loop 
+        int n = 1;
+        do{
+            System.out.println(n);
+            n++;
+        }while (n <=5);
+    }
+}
