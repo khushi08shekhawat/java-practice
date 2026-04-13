d9f0d3f27028de39532af2ee076e864f5fb3e637
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-04-13 22:13:53 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String [] args){
+        Scanner in  = new Scanner(System.in);
+// take input from user till user press x
+        int ans = 0;
+        while(true){
+            // take operator as input      
+            System.out.print("Enter the operator");
+            char op = in.next().trim().charAt(0);
+            System.out.println();
+            if (op =='+' || op == '-' || op == '*' || op == '/' || op == '%') {
+            
+                //input two numbers
+                System.out.println("Enter two numbers");
+                int num1 = in.nextInt();
+                int num2 = in.nextInt();
+                System.out.println("Your answer is");
+                if (op == '+')
+                {
+                    ans = num1 +num2;
+                }
+                if (op == '-'){
+                    ans = num1-num2;
+                    
+                }
+                if(op == '*'){
+                    ans = num1 * num2;
+
+                }
+                if (op == '/'){
+                    if (num2 != 0){
+                    ans = num1 / num2;
+                }
+                if(op == '%'){
+                    ans = num1 % num2;
+            }
+
+        } else if (op =='x' || op == 'X'){
+            break;
+        } 
+    }
+        System.out.println(ans);
+    }
+    }
+}
