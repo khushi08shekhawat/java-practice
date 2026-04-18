bef6bf0369b83383b70244c6436e920f11f8fe73
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-04-18 18:15:12 +0530

    Add files via upload

---
+++ Greetings.java
@@ -0,0 +1,13 @@
+public class Greetings {
+    public static void main (String [] args){
+        greetings();// we call the function here 
+
+    }
+    static void greetings(){// when we dont want to 
+    // return anything just want to display we use void
+     
+        System.out.println("Hello World");
+        
+    }
+    }
+
---
+++ StringGreet.java
@@ -0,0 +1,12 @@
+public class StringGreet {
+    public static void main (String []args){
+        String message = greet();
+        System.out.println(message);  
+              
+    }
+     static String greet(){
+        String greetings = "How are you";
+        return greetings;
+     } 
+}
+ 
