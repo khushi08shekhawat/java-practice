6d5578afd4a21a0043e7c25793a0cab4126742e7
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-04-25 17:05:33 +0000

    new file:   NameGreeting.class 	new file:   NameGreeting.java

---
+++ .java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class NameGreeting{
+    public static void main(String []args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is  your name?")
+        string name = in.nextString;
+        System.out.println("Hell0 KHUSHI!!!!!!" + name);
+    
+    }
+    
+}
---
+++ NameGreeting.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class NameGreeting{
+    public static void main(String []args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is  your name?");
+        String name = in.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}  
