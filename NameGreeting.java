25055e7c06cd6c3db2d0dd8e774d8431d609b6e6
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-04-26 22:52:54 +0000

    new file:   IntegerInputs.class 	new file:   IntegerInputs.java

--- NameGreeting.java
+++ NameGreeting.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 public class NameGreeting{
     public static void main(String []args){
         Scanner in = new Scanner(System.in);
-        System.out.println("What is  your name?");
-        String name = in.nextLine();
-        System.out.println("Hello, " + name + "!");
+        System.out.println("What is  your name?")
+        string name = in.nextString;
+        System.out.println("Hell0 KHUSHI!!!!!!" + name);
+    
     }
-}  
+    
+}
