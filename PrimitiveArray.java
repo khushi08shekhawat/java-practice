5e33a766c07a02c3bbaf5df2fe21cbda1e7e70ba
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-04-29 16:14:13 +0000

    new file:   PrimitiveArray.class 	new file:   PrimitiveArray.java

---
+++ PrimitiveArray.java
@@ -0,0 +1,11 @@
+public class PrimitiveArray{
+    public static void main(String[]args){
+        int[]arr = new int[5];
+        arr[0]= 45;
+        arr[1]= 34;
+        arr[2]= 57;
+        arr[3]= 54;
+        arr[4]= 91;
+        System.out.println(arr[2]);
+    }
+}
