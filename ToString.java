d8d68eff8357044e1310955c30683ee30fab7636
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-04-30 04:49:05 +0000

    new file:   ToString.class 	new file:   ToString.java

---
+++ ToString.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class ToString{
+    public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+    
+        int[]arr = new int[5];
+        arr[0]= 45;
+        arr[1]= 34;
+        arr[2]= 57;
+        arr[3]= 54;
+        arr[4]= 91;
+    
+    //array of objects
+    String [] str = new String[4];
+    for(int i = 0; i< str.length; i++){
+        str[i] = in.next();
+    }
+    System.out.println(Arrays.toString(str));
+    }
+    }
