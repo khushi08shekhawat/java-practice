0235df3e461581ac64ee6f960d72bfed4341942c
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-05-02 15:08:17 +0000

    new file:   AreaOfCircle.class 	new file:   AreaOfCircle.java

---
+++ AreaOfCircle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class AreaOfCircle{
+    public static void main(String[]args){
+
+     Scanner in = new Scanner(System.in);
+     System.out.println("Enter the value of radius");
+        double r =in.nextDouble();
+        double a = 3.14*r*r;
+        System.out.println("Area of Circle is " + a);
+     
+
+}
+}
