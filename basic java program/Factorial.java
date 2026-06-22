ad744b448371f64f5904e04f850bdb4d32f6b8fb
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-06-22 06:09:32 +0000

    new file:   basic java program/Factorial.class 	new file:   basic java program/Factorial.java

---
+++ basic java program/Factorial.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Factorial{
+    public static void main (String[] args){
+    Scanner in = new Scanner (System.in);
+    System.out.println("Enter a number");
+    int num = in.nextInt();
+    for(int i =1; i <= num; i++){
+    if(num % i == 0) {  
+        System.out.print(" " + i);
+    }
+    }
+    }
+}
