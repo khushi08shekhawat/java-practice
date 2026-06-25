b27931378a5c56d6c70c06f8d8eed6531168b760
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-06-25 06:06:51 +0000

    new file:   conditional loops/Functions2.java

---
+++ conditional loops/Functions2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Functions2{
+        public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(isArmstrong(n));
+        
+}
+        static boolean isArmstrong(int n){
+            int original = n ;
+            int sum = 0;
+            while(n > 0)
+            {
+                int rem = n % 10;
+                n /= 10;
+                sum = sum + rem*rem*rem;
+            }
+            return sum == original; //if(sum ==original){
+            //true}else{
+            //return false};
+        }
+}
