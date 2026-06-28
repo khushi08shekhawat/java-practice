12d22a7abcad0b1e47386c6f54229e4768194c72
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-06-28 06:11:36 +0000

    new file:   arrays/MaxRange.java

---
+++ arrays/MaxRange.java
@@ -0,0 +1,17 @@
+public class MaxRange{
+    public static void main(String[] args){
+        int[] arr = {23,44,56,176,123};
+        System.out.println(maxRange(arr,0,4));
+
+    }
+    static int maxRange(int[]arr, int start , int end){
+        int maxVal = arr[start];
+        for(int i = 1; i <= end; i++ ){
+            if(arr[i] > maxVal){
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+
+}
