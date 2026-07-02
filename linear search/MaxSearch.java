d203360847a5c099d58cb640c0570554b323991e
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-07-02 03:17:58 +0000

    new file:   linear search/MaxSearch.java

---
+++ linear search/MaxSearch.java
@@ -0,0 +1,17 @@
+public class MaxSearch{
+    public static void main(String[]args){
+        int[] arr ={18,12,-7,3,14,28};
+    System.out.println(max(arr));
+
+    }
+    
+    static int max(int[]arr){
+        int ans = arr[0];
+        for(int i =1; i<arr.length; i++){
+            if(arr[i] > ans){
+                ans = arr[i];
+            }
+        }
+        return ans;
+    }
+}
