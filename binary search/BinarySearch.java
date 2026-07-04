d11f83f7d10d2b713f2d5f01cac07b6756840a5c
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-07-04 05:27:40 +0000

    new file:   binary search/BinarySearch.java

---
+++ binary search/BinarySearch.java
@@ -0,0 +1,27 @@
+public class BinarySearch{
+    public static void main(String[]args){
+        int[] arr = {-81,-19,3,6,9,25,37,48,59,60};
+        int target = 6;
+        int ans = binarysearch(arr,target);
+        System.out.println(ans);
+    }
+    static int binarysearch(int[]arr, int target){
+        int start =0;
+        int end = arr.length-1;
+        
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(target<arr[mid]){
+                end =mid-1;
+            }
+            else if(target> arr[mid]){
+                start = mid +1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+    
+}
