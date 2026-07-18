d8908511321989e1577503bea16cb2efa3b4945b
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-07-18 14:26:11 +0000

    new file:   binary search/Leetcode_704.java

---
+++ binary search/Leetcode_704.java
@@ -0,0 +1,24 @@
+public class Leetcode_704{
+    public static void main(String[]args){
+    int[]arr={-1,0,3,5,9,12};
+    int target = 9;
+    int ans =binarysearch(arr,target);
+    System.out.println(ans);
+    }
+    static int binarysearch(int[]arr,int target){
+    int start =0;
+    int end =arr.length-1;
+    while(start<=end){
+        int mid = start+(end-start)/2;
+        if(target<arr[mid]){
+            end =mid-1;
+        }else if(target> arr[mid]){
+            start=mid+1;
+        }
+        else{
+            return mid;
+        }
+    }
+    return -1;
+    }
+}
