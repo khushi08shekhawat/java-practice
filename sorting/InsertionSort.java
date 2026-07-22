9395ab8b7a81ac4b71b1264f1c12125e1c5202b1
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-07-22 14:29:45 +0000

    new file:   sorting/InsertionSort.java

---
+++ sorting/InsertionSort.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[]args){
+        int[]arr={3,1,5,4,2};
+        selectionsort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void insertionsort(int[]arr){
+        for(int i =0;i<arr.length;i++){
+            for(int j = i+1;j>0;j--){
+                if(arr[j]<arr[j-1]){
+                    swap(arr,j,j-1);
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    }
+
+    static void selectionsort(int[]arr){
+     for(int i =0;i<arr.length;i++){
+    // find the max item in the remaining array and swap with correct index
+    int last = arr.length - i - 1;
+    int maxIndex = getMaxIndex(arr,0, last);
+    swap(arr,maxIndex,last);
+}
+} 
+        static void swap(int[]arr,int first,int second){
+            int temp=arr[first];
+            arr[first]=arr[second];
+            arr[second]=temp;
+        }
+
+    static int getMaxIndex(int[]arr,int start,int end){
+        int max = start;
+        for(int i = start;i<=end;i++){
+            if(arr[max]<arr[i]){
+                max=i;
+            }
+        }
+        return max;
+    }
+    static void bubblesort(int[]arr){
+        boolean swapped;
+ //we are using void becoz no new array is being created 
+ //reun the steps n-1 times
+    for(int i =0;i<arr.length;i++){
+        swapped=false;
+    for(int j =1;j<arr.length-i;j++){
+        //swap if the item is smaller than the previous one
+        if(arr[j]<arr[j-1]){
+
+            //swap
+            int temp=arr[j];
+            arr[j]=arr[j-1];
+            arr[j-1]=temp;
+            swapped=true;
+        }
+    }
+    //if you did not swap for  particular value of i , it means the arr is sorted hence stop the program
+    if(swapped==false){
+        break;//not false =true
+    
+}
+
+}
+}
+}
