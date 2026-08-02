5ff0662332f701511e527373dedc3ca41fc9a866
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-08-02 23:36:46 +0000

    modified:   sorting/CyclicSort.java

--- sorting/CyclicSort.java
+++ sorting/CyclicSort.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class CyclicSort {
     public static void main (String[] args){
-     int[]arr={3,5,2,1,4};
+     int[]arr={5,4,3,1,2};
      sort(arr);
      System.out.println(Arrays.toString(arr));   
     }
