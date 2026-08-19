ab1d31602fb237103b7e12c8e62939cbb1b836a5
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-08-20 03:50:47 +0530

    new file:   interview/Arreverse.java

---
+++ interview/Arreverse.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+public class Arreverse {
+    public static void main(String[]args){
+        int[]arr={10,25,7,45,18};
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+         static void reverse(int[]arr){
+        int start =0;
+        int end = arr.length-1;
+        while(start<end){
+            int temp=arr[start];
+            arr[start]= arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+
+
+}
