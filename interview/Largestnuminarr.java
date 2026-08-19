a8902e7f23e532d153d3d90d699db1c90fe44691
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-08-20 03:28:13 +0530

    new file:   interview/Largestnuminarr.java

---
+++ interview/Largestnuminarr.java
@@ -0,0 +1,12 @@
+public class Largestnuminarr {
+    public static void main(String[]args){
+        int[]arr={10,25,7,45,18};
+        int max = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                max =arr[i];
+            }
+        }
+        System.out.println("Largest number:"+max); 
+       }
+}
