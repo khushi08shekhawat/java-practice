df448bda01dd59fc70f649b5ea7a4e7f6be663a5
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-08-20 03:32:08 +0530

    new file:   interview/Smallestnuminarr.java

---
+++ interview/Smallestnuminarr.java
@@ -0,0 +1,12 @@
+public class Smallestnuminarr {
+    public static void main(String[]args){
+        int[]arr={10,25,7,45,18};
+        int min = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]<min){
+                min =arr[i];
+            }
+        }
+        System.out.println("Smallest number:"+min); 
+       }
+}
