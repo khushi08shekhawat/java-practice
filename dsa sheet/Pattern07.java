0c7bb8623899ffbbc3f06f8055a65941f3bddd79
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-08-23 18:33:37 +0530

    modified:   dsa sheet/Pattern07.java

--- dsa sheet/Pattern07.java
+++ dsa sheet/Pattern07.java
@@ -1,9 +1,10 @@
 public class Pattern07 {
     public static void main(String[] args) {
     int n =5;
-    //spaces
     for(int row=1;row<=n;row++){
-         System.out.print(" ");
+        //spaces
+        for(int col=1;col<=n-row-1;col++)
+        System.out.print(" ");
 
             //stars
         for(int col=1;col<=2*row-1;col++)
