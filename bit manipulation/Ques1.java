5ab9a5827ceb01d2ce28d41d01c521e7fdb27b95
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-08-24 17:30:11 +0530

    new file:   bit manipulation/Ques1.java

---
+++ bit manipulation/Ques1.java
@@ -0,0 +1,11 @@
+public class Ques1 {
+    public static void main(String[]args){
+        int n = 22;
+        if((n & 1) == 1){
+            System.out.println("odd");
+        }else{
+            System.out.println("even");
+        }
+
+    }
+}
