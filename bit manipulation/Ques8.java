11e7ee9fa0e332bfe2cf540174ba369cb7dbd973
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-08-27 21:02:21 +0530

    new file:   bit manipulation/Ques8.java

---
+++ bit manipulation/Ques8.java
@@ -0,0 +1,9 @@
+public class Ques8 {
+    public static void main(String[]args){
+        int[] arr={2, 2, 3, 2, 7, 7,8, 7,8,8 };
+         for (int i = 0; i < 4; i++) {
+
+            System.out.println(1 << i);
+    }
+}
+}
