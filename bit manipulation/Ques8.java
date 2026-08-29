d3a50e3ffcf5bc99b931f71fcdbedb6f1adb0076
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-08-29 11:27:19 +0530

    modified:   bit manipulation/Ques8.java

--- bit manipulation/Ques8.java
+++ bit manipulation/Ques8.java
@@ -1,9 +1,21 @@
 public class Ques8 {
     public static void main(String[]args){
         int[] arr={2, 2, 3, 2, 7, 7,8, 7,8,8 };
-         for (int i = 0; i < 4; i++) {
+        int ans =0;
+         for (int i = 0; i < 32; i++) {
+        int count =0;
+        for(int num =0;num<arr.length;num++){
+            if((arr[num] &(1<<i))!=0){
+                count++;
+            }
+        }
+            if(count %3!=0){
+                ans =ans | (1<<i);
 
-            System.out.println(1 << i);
+            }
+        
     }
+            System.out.println(ans);
+    
 }
 }
