ad8dd9e2d1e2df439c3f6432cf5030d2e2fa161d
Khushi Shekhawat <khushishekhawat0811@gmail.com>
2026-08-30 11:29:18 +0530

    new file:   bit manipulation/Ques10.java

---
+++ bit manipulation/Ques10.java
@@ -0,0 +1,8 @@
+public class Ques10 {
+    public static void main(String[]args){
+        int n =7;
+        int b=2;
+        int ans =(int)(Math.log(n)/Math.log(b))+1;
+        System.out.println(ans);
+    }
+}
